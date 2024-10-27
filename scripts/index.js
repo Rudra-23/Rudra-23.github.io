8f34946215408d6dcd38131a8bea3bc2d741bb6e
Rudra Gujarathi <rudra.gujarathi123@gmail.com>
2024-10-27 16:36:42 -0700

    porfolio created

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+
+    const items = document.querySelector('.items');
+    const links = document.querySelector('.links');
+
+    items.classList.toggle('show');
+    links.classList.toggle('show');
+}
